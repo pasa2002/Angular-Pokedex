f78035ea1dc539de13a330a26fc2678420559950
Sanjaya Shrestha <sanjaya12stha@gmail.com>
2023-11-15 11:29:53 +0100

    service-component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MainComponent } from './main/main.component';
+import { CardComponent } from './card/card.component';
+import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavBarComponent,
+    MainComponent,
+    CardComponent,
+    PokemonDetailComponent
   ],
   imports: [
     BrowserModule,
